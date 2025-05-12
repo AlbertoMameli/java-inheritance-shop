8003bc45c518990868d5004a8bfaa1d8975048ac
AlbertoMameli <mamelialberto2@gmail.com>
2025-05-12 16:04:22 +0200

    feat: inizio i setter e getter, poi inserirò dei controlli validi

--- org/lesson/Televisione.java
+++ org/lesson/Televisione.java
@@ -5,15 +5,26 @@ import org.lesson.java.inheritance.shop.Prodotto;
 public class Televisione extends Prodotto {
     private int pollici;
     private boolean smartTv;
-    public Televisione(String nome, String marca, float prezzo, float iva,int pollici, boolean smartTv){
+
+    public Televisione(String nome, String marca, float prezzo, float iva, int pollici, boolean smartTv) {
         super(nome, marca, marca, prezzo, iva);
         this.pollici = pollici;
         this.smartTv = smartTv;
 
     }
 
-    public boolean isSmartTv(){
+    // getter
+    public boolean isSmartTv() {
         return this.smartTv;
     }
-    
+
+    public int getPollici() {
+        return this.pollici;
+    }
+
+    // setter
+    public void setPollici( int pollici){
+        this.pollici =  pollici;
+    }
+
 }
--- org/lesson/java/inheritance/shop/Smartphone.java
+++ org/lesson/java/inheritance/shop/Smartphone.java
@@ -1,7 +1,9 @@
 package org.lesson.java.inheritance.shop;
+import java.util.Random;
+
 import org.lesson.java.inheritance.shop.Prodotto;
 //Smarphone, caratterizzati anche dal codice IMEI e dalla quantità di memoria
-public class Smartphone extends Prodotto {  //estendo la la classe smatphone alla classe prodotto e aggiungo le caratteristiche dell'oggetto da creare
+public class Smartphone extends Prodotto {  //estendo la la classe smartphone alla classe prodotto e aggiungo le caratteristiche dell'oggetto da creare
     private String imei;
     private int memoria;
 
@@ -13,8 +15,37 @@ public class Smartphone extends Prodotto {  //estendo la la classe smatphone all
 
     }
 
+    //getter per leggere i valori
+
     public String getCodiceImei(){
         return this.imei;
     }
+    public int getMemoria(){
+        return this.memoria;
+    }
 
+
+    //setter per controllare i valori
+    public void setImei (String imei){
+        this.imei = imei;
+    }
+
+    public void setMemoria (int memoria){
+        this.memoria = memoria;
+    }
+
+
+    //genera imei randomico
+    public void generaImei(){
+        Random rand = new Random();
+        StringBuilder generatedImei = new StringBuilder();  //creo un contenitore vuoto per contenere i numeri che andrò a generare
+        for (int i = 0; i < 15; i ++){
+            generatedImei.append(rand.nextInt(10));//genero numero casuale tra 0 e 10 e lo aggiungo con append alla stringa
+        }
+        this.imei = generatedImei.toString(); //convertiamo stringbuilder in stringa
+    }
+@Override //implementiamo un metodo della sottoclasse alla nostra superclasse
+public String toString(){
+    return super.toString() + " " + imei + " " + memoria + " GB";
+}
 }
